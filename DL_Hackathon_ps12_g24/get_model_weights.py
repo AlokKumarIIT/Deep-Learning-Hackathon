2a114b575b90c51076fb628671a4271bcf650abf
Karangera07 <karangera205@gmail.com>
2026-04-19 06:57:01 +0530

    Video_Gen_Dis

--- DL_Hackathon_ps12_g24/get_model_weights.py
+++
@@ -1,6 +0,0 @@
-#from PIL import Image
-#print(hasattr(Image, "Resampling"))
-
-from transformers import Wav2Vec2Model
-
-model=Wav2Vec2Model.from_pretrained("facebook/wav2vec2-large-960h",cache_dir="./models")
--- m4aTowav.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-audio = AudioSegment.from_file("long_record1.m4a")
-audio.export("long_record1.wav", format="wav")
-audio = AudioSegment.from_file("long_record2.m4a")
-audio.export("long_record2.wav", format="wav")
-
-print("Converted successfully!")
